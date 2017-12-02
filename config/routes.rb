7786da4cef611ee5361a1c9c293322a6b688f6f0
= <mmps@cin.ufpe.br>
2017-12-02 15:31:00 -0200

    pronto para desenvolvimento das telas

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   resources :telfuncionarios
   resources :funcionarios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'general#home'
 end
